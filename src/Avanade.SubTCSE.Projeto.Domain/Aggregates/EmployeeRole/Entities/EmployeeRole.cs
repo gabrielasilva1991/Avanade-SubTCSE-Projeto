3fe31545dd97c696055bc652511e0a6d204fc2b9
gabrielasilva1991 <gabriela.silva1991@outlook.com>
2021-06-17 11:59:10 -0300

    aula 02: creating csproj, aula 03, aula 04

---
+++ src/Avanade.SubTCSE.Projeto.Domain/Aggregates/EmployeeRole/Entities/EmployeeRole.cs
@@ -0,0 +1,20 @@
+﻿namespace Avanade.SubTCSE.Projeto.Domain.Aggregates.EmployeeRole.Entities
+{
+    public class EmployeeRole
+    {
+        public EmployeeRole(string id, string roleName)
+        {
+            Id = id; //ctrl + . - clica em generate constructor abre janela pick members - clica ok
+            RoleName = roleName;
+        }
+
+        public EmployeeRole(string roleName)
+        {
+            RoleName = roleName;
+        }
+
+        public string Id { get; private set; } //escreve prop e da 2 tab - vai auto completar
+
+        public string RoleName { get; set; }
+    }
+} //ctrl + k + e identa o código
