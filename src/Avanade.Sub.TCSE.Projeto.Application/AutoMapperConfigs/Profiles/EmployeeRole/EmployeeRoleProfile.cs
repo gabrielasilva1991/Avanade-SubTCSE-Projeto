6717dd3e6ec847b381d94ec5ab6f268a0e38d544
gabrielasilva1991 <gabriela.silva1991@outlook.com>
2021-06-23 10:59:57 -0300

    aula 06

--- src/Avanade.Sub.TCSE.Projeto.Application/AutoMapperConfigs/Profiles/EmployeeRole/EmployeeRoleProfile.cs
+++ src/Avanade.Sub.TCSE.Projeto.Application/AutoMapperConfigs/Profiles/EmployeeRole/EmployeeRoleProfile.cs
@@ -6,8 +6,14 @@ namespace Avanade.Sub.TCSE.Projeto.Application.AutoMapperConfigs.Profiles.Employ
     {
         public EmployeeRoleProfile()
         {
-          CreateMap<Dtos.EmployeeRole.EmployeeRoleDto, SubTCSE.Projeto.Domain.Aggregates.EmployeeRole.Entities.EmployeeRole>()
-                .ForCtorParam("roleName", opt => opt.MapFrom(src => src.Cargo));
+            CreateMap<Dtos.EmployeeRole.EmployeeRoleDto, SubTCSE.Projeto.Domain.Aggregates.EmployeeRole.Entities.EmployeeRole>()
+                  .ForCtorParam("roleName", opt => opt.MapFrom(src => src.Cargo));
+
+            CreateMap<SubTCSE.Projeto.Domain.Aggregates.EmployeeRole.Entities.EmployeeRole, Dtos.EmployeeRole.EmployeeRoleDto>()
+                .ForMember(dest => dest.Identificador, opt => opt.MapFrom(src => src.Id))
+                .ForMember(dest => dest.Cargo, opt => opt.MapFrom(src => src.RoleName))
+                .ForAllOtherMembers(i => i.Ignore());
+
         }
     }
 }
--- src/Avanade.SubTCSE.Projeto.Domain/Aggregates/EmployeeRole/Entities/EmployeeRole.cs
+++ src/Avanade.SubTCSE.Projeto.Domain/Aggregates/EmployeeRole/Entities/EmployeeRole.cs
@@ -1,10 +1,10 @@
 ﻿namespace Avanade.SubTCSE.Projeto.Domain.Aggregates.EmployeeRole.Entities
 {
-    public record EmployeeRole
+    public record EmployeeRole : BaseEntity<string>
     {
         public EmployeeRole(string id, string roleName)
         {
-            Id = id; //ctrl + . - clica em generate constructor abre janela pick members - clica ok
+            Id = id;
             RoleName = roleName;
         }
 
@@ -13,8 +13,6 @@
             RoleName = roleName;
         }
 
-        public string Id { get; init; } //escreve prop e da 2 tab - vai auto completar
-
         public string RoleName { get; init; }
     }
-} //ctrl + k + e identa o código
+}
