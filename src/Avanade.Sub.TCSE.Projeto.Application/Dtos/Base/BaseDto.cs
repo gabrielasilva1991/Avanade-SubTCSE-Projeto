01ab37b124db7c8a6a3e2de6196b1a8cf8d41d91
gabrielasilva1991 <gabriela.silva1991@outlook.com>
2021-07-29 12:24:29 -0300

    aula 09

--- src/Avanade.Sub.TCSE.Projeto.Application/Dtos/Base/BaseDto.cs
+++ src/Avanade.Sub.TCSE.Projeto.Application/Dtos/Base/BaseDto.cs
@@ -1,8 +1,12 @@
-﻿namespace Avanade.SubTCSE.Projeto.Application.Dtos.Base
+﻿using FluentValidation.Results;
+
+namespace Avanade.SubTCSE.Projeto.Application.Dtos.Base
 {
     public abstract class BaseDto
     {
-        public int Identificador { get; set; }
+        public string Identificador { get; set; }
+
+        public ValidationResult ValidationResult { get; set; }
 
     }
 }
--- src/Avanade.Sub.TCSE.Projeto.Application/Services/EmployeeRole/EmployeeRoleAppService.cs
+++ src/Avanade.Sub.TCSE.Projeto.Application/Services/EmployeeRole/EmployeeRoleAppService.cs
@@ -1,15 +1,30 @@
 ﻿using Avanade.SubTCSE.Projeto.Application.Interfaces.EmployeeRole;
 using Avanade.SubTCSE.Projeto.Application.Dtos.EmployeeRole;
 using System.Threading.Tasks;
+using AutoMapper;
+using Avanade.SubTCSE.Projeto.Domain.Aggregates.EmployeeRole.Interfaces.Services;
 
 namespace Avanade.SubTCSE.Projeto.Application.Services.EmployeeRole
 {
     public class EmployeeRoleAppService : IEmployeeRoleAppService
     {
-        public Task<EmployeeRoleDto> AddEmployeeRole(EmployeeRoleDto employeeRoleDto)
-        {
-            throw new System.NotImplementedException(); //linha comentada para teste build
+        private readonly IMapper _mapper;
 
+        private readonly IEmployeeRoleService _employeeRoleService;
+
+        public async Task<EmployeeRoleDto> AddEmployeeRoleAsync(EmployeeRoleDto employeeRoleDto)
+        {
+            //mapear
+            var itemDomain = _mapper.Map<EmployeeRoleDto, Domain.Aggregates.EmployeeRole.Entities.EmployeeRole>(employeeRoleDto);
+
+            //chamar metodo
+            var item = await _employeeRoleService.AddEmployeeRoleAsync(itemDomain);
+
+            //mapear
+            var itemDto = _mapper.Map<Domain.Aggregates.EmployeeRole.Entities.EmployeeRole, EmployeeRoleDto>(item);
+
+            //devolver
+            return itemDto; 
         }
     }
 }
